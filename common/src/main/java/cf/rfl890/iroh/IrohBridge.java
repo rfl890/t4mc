bab2fc6e4765635e1b6cf46626bcff7917e89c0c
rfl890 <rfl890mc@gmail.com>
2026-06-06 16:44:02 -0400

    Version bump - Adjustments to native library loading code to waste less disk space

--- common/src/main/java/cf/rfl890/iroh/IrohBridge.java
+++ common/src/main/java/cf/rfl890/iroh/IrohBridge.java
@@ -1,11 +1,13 @@
 package cf.rfl890.iroh;
 
 public class IrohBridge {
-    public static native String tcp2Iroh(String address, short port);
-    public static native long iroh2Tcp(String address, String ticket, short port);
-    public static native void shutdown(long handle);
-
     static {
         IrohNativeLoader.init();
     }
+
+    public static native String tcp2Iroh(String address, short port);
+
+    public static native long iroh2Tcp(String address, String ticket, short port);
+
+    public static native void shutdown(long handle);
 }
